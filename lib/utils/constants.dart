9aed5d6f35a0a74bb6e6df43647bb1b74a852288
Gabriel <gabriel@gabrielpro.nl>
2024-06-12 14:19:34 +0300

    from local to online

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,2 @@
-const String baseUrl = 'http://192.168.43.230:3000/api';
-// const String baseUrl = 'http://abasi.duckdns.org:3000/api';
+// const String baseUrl = 'http://192.168.43.230:3000/api';
+const String baseUrl = 'http://abasi.duckdns.org:3000/api';
